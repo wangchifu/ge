c117cd590533708d4911db68b49e2def05dc6d3e
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-04-24 20:07:36 +0800

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,8 @@
           <a class="d-inline-block mb-4 pb-2" href="index.html">
             <img loading="prelaod" decoding="async" class="img-fluid" src="{{ asset('images/logo-white.png') }}" alt="Reporter Hugo">
           </a>
-          <p class="text-secondary">尊重每個性別，打破刻板框架，讓平等在生活中自然發聲。</p>
+          <p class="text-light">尊重每個性別，打破刻板框架，讓平等在生活中自然發聲。</p>
+          <p class="text-light">Copyright © 彰化縣政府教育處 學特科 {{ date('Y') }}</p>
         </div>
       </div>
     </div>
