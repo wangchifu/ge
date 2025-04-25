3ff7e92ef6575975983ee36d2c995546dc9fa399
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-04-25 22:05:46 +0800

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container section">
       <div class="row">
         <div class="col-lg-10 mx-auto text-center">
-          <a class="d-inline-block mb-4 pb-2" href="index.html">
+          <a class="d-inline-block mb-4 pb-2" href="{{ route('index') }}">
             <img loading="prelaod" decoding="async" class="img-fluid" src="{{ asset('images/logo-white.png') }}" alt="Reporter Hugo">
           </a>
           <p class="text-light">尊重每個性別，打破刻板框架，讓平等在生活中自然發聲。</p>
